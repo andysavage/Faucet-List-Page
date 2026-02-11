a252d73c5eb446c18837047e42f62c21c8a907d5
andysavage <web@directsponsor.org>
2026-02-11 23:08:59 +0000

    faucetlist deploy - 2026-02-11 23:08:59

--- site/index.php
+++ site/index.php
@@ -499,7 +499,7 @@ $floatingAd = getRandomAd('ads-floating.txt');
     </div>
 
     <div id="top-image"
-        style="text-align:center; margin: 0 auto 20px auto; min-height: 90px; max-width: 728px; display: flex; justify-content: center; align-items: center;">
+        style="text-align:center; margin: 0 auto 20px auto; max-width: 728px; aspect-ratio: 728 / 90; display: flex; justify-content: center; align-items: center; overflow: hidden;">
         <?php echo $bannerAd; ?>
     </div>
 
